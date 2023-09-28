9ae1de30ff1e49f7b8f951d715e2100595c74123
rohanharikr <rohanharikumar80@gmail.com>
2023-09-29 01:07:30 +0530

    send button props for testing

--- app/page.jsx
+++ app/page.jsx
@@ -18,7 +18,7 @@ export default function Home() {
                 <Hero/>
                 
                 {/* Continue with Hellō button */}
-                <ContinueButton/> 
+                <ContinueButton targetURI="/" providerHint="facebook --google" /> 
 
                 <Divider/>
                 <Description/>
@@ -36,13 +36,13 @@ export default function Home() {
                         {/* Use next/image */}
                         <img src={user?.picture} className="h-24 rounded-full bg-gray-700" />
                         {/* Update Pictutre with Hellō button */}
-                        <UpdatePictureButton/> 
+                        <UpdatePictureButton targetURI="/" providerHint="github --google"/> 
                     </ProfileSection>
 
                     <ProfileSection>
                         <h2 className="text-xl text-left">{user?.email}</h2>
                         {/* Update Email with Hellō button */}
-                        <UpdateEmailButton/> 
+                        <UpdateEmailButton targetURI="/" providerHint="microsoft --facebook"/> 
                     </ProfileSection>
                 </Profile>
             </LoggedIn>
