5ce111ef76dcfe0504b5d1a80351f6d38cb1c2ce
rohanharikr <rohanharikumar80@gmail.com>
2023-09-29 20:16:07 +0530

    console page

--- app/page.jsx
+++ app/page.jsx
@@ -5,9 +5,18 @@ import Divider from "@/components/Divider"
 import Description from "@/components/Description"
 import ExamplesTable from "@/components/ExamplesTable"
 import LinkCards from "@/components/LinkCards"
-import { ContinueButton, LoggedIn, LoggedOut, UpdateEmailButton, UpdatePictureButton, useUser } from "@hellocoop/nextjs"
+import { ContinueButton, LoggedIn, LoggedOut, useUser } from "@hellocoop/nextjs"
+import { useEffect } from "react"
+import { getConsoleURI } from "../lib/utils.js"
+import { useRouter } from "next/navigation"
 
 export default function Home() {
+    const { replace } = useRouter()
+    useEffect(() => {
+        if(getConsoleURI())
+            return replace('/console' + window.location.search)
+    }, [])
+
     const user = useUser()
     return (
         <>
