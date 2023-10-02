ea5d15e381a319177f237204ff26039976846a75
rohanharikr <rohanharikumar80@gmail.com>
2023-10-02 17:40:21 +0530

    update link cards design

--- components/LinkCards.jsx
+++ components/LinkCards.jsx
@@ -1,18 +1,29 @@
+import Link from "next/link";
+
+const links = [
+    {
+        text: "Learn more about Hellō",
+        link: "https://www.hello.coop"
+    },
+    {
+        text: "Read the Hellō Developer Documentation",
+        link: "https://www.hello.dev"
+    },
+    {
+        text: "View the source code for the starter app",
+        link: "https://www.github.com/hellocoop/hello-nextjs-starter"
+    },
+]
+
 export default function LinkCards() {
     return (
         <section className="flex flex-wrap justify-between mt-10">
-            <div className="w-56 h-36 border border-white border-opacity-25 rounded-md text-left flex flex-col items-start justify-between p-4">
-                <span>Learn more<br/>about Hellō</span>
-                <span>www.hello.coop</span>
-            </div>
-            <div className="w-56 h-36 border border-white border-opacity-25 rounded-md text-left flex flex-col items-start justify-between p-4">
-                <span>Learn more<br/>about Hellō</span>
-                <span>www.hello.coop</span>
-            </div>
-            <div className="w-56 h-36 border border-white border-opacity-25 rounded-md text-left flex flex-col items-start justify-between p-4">
-                <span>Learn more<br/>about Hellō</span>
-                <span>www.hello.coop</span>
-            </div>
+            {links.map(i => 
+                <Link href={i.link} key={i.link} className="cursor-pointer w-72 h-32 border dark:border-[white] border-[#303030] dark:border-opacity-25 border-opacity-25 hover:border-opacity-60 dark:hover:border-opacity-25 rounded-lg text-left flex flex-col items-start justify-between p-4 dark:hover:bg-[#202020] transition-color duration-200">
+                    <span className="font-medium text-lg">{i.text}</span>
+                    <span className="opacity-80">{new URL(i.link).hostname}</span>
+                </Link>    
+            )}
         </section>
     )
 }
