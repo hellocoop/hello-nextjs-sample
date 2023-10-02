0a95fd59f2eb861b5e6135909d69b04e87566d47
rohanharikr <rohanharikumar80@gmail.com>
2023-10-02 17:46:50 +0530

    open link card in new tab

--- components/LinkCards.jsx
+++ components/LinkCards.jsx
@@ -19,7 +19,7 @@ export default function LinkCards() {
     return (
         <section className="flex flex-wrap justify-between mt-10">
             {links.map(i => 
-                <Link href={i.link} key={i.link} className="cursor-pointer w-72 h-32 border dark:border-[white] border-[#303030] dark:border-opacity-25 border-opacity-25 hover:border-opacity-60 dark:hover:border-opacity-25 rounded-lg text-left flex flex-col items-start justify-between p-4 dark:hover:bg-[#202020] transition-color duration-200">
+                <Link href={i.link} target="_blank" key={i.link} className="link-card cursor-pointer w-72 h-32 border dark:border-[white] border-[#303030] dark:border-opacity-25 border-opacity-25 hover:border-opacity-60 dark:hover:border-opacity-25 rounded-lg text-left flex flex-col items-start justify-between p-4 dark:hover:bg-[#202020] transition-color duration-200">
                     <span className="font-medium text-lg">{i.text}</span>
                     <span className="opacity-80">{new URL(i.link).hostname}</span>
                 </Link>    
