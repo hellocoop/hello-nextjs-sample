a964a07a51f921348691c88bdae57c3716c7bd58
rohanharikr <rohanharikumar80@gmail.com>
2023-10-02 15:04:10 +0530

    remove console page

--- app/page.jsx
+++ app/page.jsx
@@ -3,21 +3,10 @@
 import Hero from "@/components/Hero"
 import Divider from "@/components/Divider"
 import Description from "@/components/Description"
-import ExamplesTable from "@/components/ExamplesTable"
 import LinkCards from "@/components/LinkCards"
-import { ContinueButton, LoggedIn, LoggedOut, useUser } from "@hellocoop/nextjs"
-import { useEffect } from "react"
-import { getConsoleURI } from "../lib/utils.js"
-import { useRouter } from "next/navigation"
+import { ContinueButton, LoggedIn, LoggedOut } from "@hellocoop/nextjs"
 
 export default function Home() {
-    const { replace } = useRouter()
-    useEffect(() => {
-        if(getConsoleURI())
-            return replace('/console' + window.location.search)
-    }, [])
-
-    const user = useUser()
     return (
         <>
             <Hero/>
@@ -33,7 +22,6 @@ export default function Home() {
 
             <Divider/>
             <Description/>
-            <ExamplesTable/>
             <LinkCards/>
         </>
     )
