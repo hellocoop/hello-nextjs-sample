b404f1c86e4a6ab5702a4fd2d43ebc7f5c912b31
rohanharikr <rohanharikumar80@gmail.com>
2023-10-03 00:08:21 +0530

    dropdown menu

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,22 @@
 'use client'
 
+import { useEffect, useState } from "react"
 import Link from "next/link"
 import Image from "next/image"
-import { LoggedIn, logOutRoute } from "@hellocoop/nextjs"
+import { LoggedIn, logOutRoute, useUser } from "@hellocoop/nextjs"
 import { usePathname } from "next/navigation"
 
 export default function Header() {
     const pathname = usePathname()
+    const user = useUser()
+    
+    const [ menu, setMenu ] = useState(false)
+
+    useEffect(() => {
+        //Close dropdown on route change
+        return () => setMenu(false)
+    }, [pathname])
+
     return (
         <header className="h-12 w-full bg-[#303030] flex-shrink-0 px-4">
             <div className="max-w-4xl h-full mx-auto flex items-center justify-between">
@@ -17,10 +27,25 @@ export default function Header() {
                 </Link>
 
                 <LoggedIn>
-                    <nav className="flex items-center space-x-4">
-                        {pathname !== "/profile" && <Link href="/profile">Profile</Link>}
-                        <Link href={logOutRoute}>Log Out</Link>
-                    </nav>
+                    <div className="flex justify-end relative">
+                        <button onClick={()=>setMenu(!menu)} className="flex items-center space-x-2">
+                            {/* Use next/image */}
+                            <img src={user?.picture} alt={user?.name} className="rounded-full h-7" />
+                            <span>{user?.email}</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className={`h-4 ${menu ? "rotate-180" : ""}`}>
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                            </svg>
+                        </button>
+                        {menu && (
+                            <>
+                                <nav className="absolute top-8 bg-[#303030] p-4 flex flex-col items-end space-y-4 w-64 md:w-full z-20">
+                                    {pathname !== "/profile" && <Link href="/profile">Profile</Link>}
+                                    <Link href={logOutRoute}>Log Out</Link>
+                                </nav>
+                                <span onClick={()=>setMenu(false)} className="h-screen w-screen left-0 fixed z-10 top-12 bg-black bg-opacity-80"></span>
+                            </>
+                        )}
+                    </div>
                 </LoggedIn>
             </div>
         </header>
