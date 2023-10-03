e4db50ee6027f53f110af1723673c6870f8c0a57
rohanharikr <rohanharikumar80@gmail.com>
2023-10-03 14:25:22 +0530

    dropdown menu mobile viewport

--- components/Header.jsx
+++ components/Header.jsx
@@ -31,16 +31,17 @@ export default function Header() {
                         <button onClick={()=>setMenu(!menu)} className="flex items-center space-x-2">
                             {/* Use next/image */}
                             <img src={user?.picture} alt={user?.name} className="rounded-full h-7" />
-                            <span>{user?.email}</span>
+                            <span className="hidden md:block">{user?.email}</span>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className={`h-4 ${menu ? "rotate-180" : ""}`}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                             </svg>
                         </button>
                         {menu && (
                             <>
-                                <nav className="absolute top-8 bg-[#303030] p-4 flex flex-col items-end space-y-4 w-64 md:w-full z-20">
-                                    {pathname !== "/profile" && <Link href="/profile">Profile</Link>}
-                                    <Link href={logOutRoute}>Log Out</Link>
+                                <nav className="absolute top-8 bg-[#303030] px-6 py-2 flex flex-col items-end md:w-full z-20">
+                                    <span className="md:hidden opacity-60 py-2">{user?.email}</span>
+                                    {pathname !== "/profile" && <Link href="/profile" className="py-2">Profile</Link>}
+                                    <Link href={logOutRoute} className="py-2">Log Out</Link>
                                 </nav>
                                 <span onClick={()=>setMenu(false)} className="h-screen w-screen left-0 fixed z-10 top-12 bg-black bg-opacity-80"></span>
                             </>
