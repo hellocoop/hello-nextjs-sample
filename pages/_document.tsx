49569edf65437bed2033d2f8f7a0b762b05b3292
rohanharikr <rohanharikumar80@gmail.com>
2023-10-04 14:48:38 +0530

    title and meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,9 @@ export default function Document() {
     return (
         <Html>
             <Head>
+                <title>Hellō Next.js Starter</title>
+                <meta name="title" content="Hellō Next.js Starter"/>
+                <meta name="description" content="Login and registration in seconds"/>
                 <link href="https://cdn.hello.coop/css/hello-btn.css" rel="stylesheet" />
             </Head>
             <body>
