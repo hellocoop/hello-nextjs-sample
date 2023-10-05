7b0fe00426667168ceb7d9b2f328583dca5646fe
rohanharikr <rohanharikumar80@gmail.com>
2023-10-06 01:06:00 +0530

    remove local nextjs pkg

--- hellocoop-nextjs/frontend/logout.tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router";
-import { logoutApiRoute } from "../lib/config";
-
-export const logOutRoute = logoutApiRoute
-
-export function logOut() {
-    const { push } = useRouter()
-    push(logoutApiRoute)
-}
--- hellocoop-nextjs/lib/api.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiHandler } from 'next'
-import { withIronSessionApiRoute } from 'iron-session/next'
-
-import { sessionOptions } from '../lib/config'
-
-export const withHelloApiRoute = ( handler: NextApiHandler ) => {
-    withIronSessionApiRoute(handler, sessionOptions)
-}
