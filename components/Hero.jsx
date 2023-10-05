afa5f63f9e2cc5e138d4c8c32b118f1e701fc073
rohanharikr <rohanharikumar80@gmail.com>
2023-10-05 17:50:40 +0530

    cobe

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,3 +1,5 @@
+import createGlobe from "cobe";
+import { useEffect, useRef } from "react";
 import Image from "next/image";
 
 export default function Hero({isLoggedIn, name}) {
@@ -23,8 +25,8 @@ export default function Hero({isLoggedIn, name}) {
             {isLoggedIn && (
                 <>
                     <section className="relative h-64 bg-no-repeat bg-center bg-contain flex items-center justify-center text-[4vw] xs:text-2xl font-mono my-16">
-                        Say Hellō to a Co-operative World<span className="blinker"></span>
-                        <Image src="/world.png" alt="Hellō to a new World" className="absolute opacity-90" width={260} height={256}/>
+                        <span className="z-20" style={{mixBlendMode: "difference"}}>Say Hellō to a Co-operative World<span className="blinker"></span></span>
+                        <Globe/>
                     </section>
 
                     <h1 className="text-2xl">{name}</h1>
@@ -32,4 +34,50 @@ export default function Hero({isLoggedIn, name}) {
             )}
         </section>
     )
+}
+
+
+function Globe() {
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    let phi = 0;
+
+    const globe = createGlobe(canvasRef.current, {
+      devicePixelRatio: 2,
+      width: 375 * 2,
+      height: 375 * 2,
+      phi: 0,
+      theta: 0,
+      dark: 1,
+      diffuse: 1.2,
+      mapSamples: 16000,
+      mapBrightness: 6,
+      baseColor: [0.3, 0.3, 0.3],
+      markerColor: [0.1, 0.8, 1],
+      glowColor: [1, 1, 1],
+      markers: [
+        // longitude latitude
+        // { location: [37.7595, -122.4367], size: 0.03 },
+        // { location: [40.7128, -74.006], size: 0.1 }
+      ],
+      onRender: (state) => {
+        // Called on every animation frame.
+        // `state` will be an empty object, return updated params.
+        state.phi = phi;
+        phi += 0.005;
+      }
+    });
+
+    return () => {
+      globe.destroy();
+    };
+  }, []);
+
+  return (
+    <canvas
+        ref={canvasRef}
+        style={{ width: 375, height: 375, maxWidth: "100%", aspectRatio: 1, margin: 'auto', position: 'absolute', zIndex: 10 }}
+    />
+  )
 }
