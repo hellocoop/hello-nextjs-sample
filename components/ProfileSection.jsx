86a09dff3c23f6a9d2983dd33e57caa41f5ed489
rohanharikr <rohanharikumar80@gmail.com>
2023-10-05 20:24:00 +0530

    remove unused component

--- components/ProfileSection.jsx
+++
@@ -1,7 +0,0 @@
-export default function ProfileSection({ children }) {
-    return (
-        <div className="flex flex-wrap items-center justify-between gap-4">
-            {children}
-        </div>
-    )
-}
