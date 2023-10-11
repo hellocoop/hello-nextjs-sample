84dd24f24ebfa83f28a1018bf70eb3d951842fa0
rohanharikr <rohanharikumar80@gmail.com>
2023-10-11 15:04:54 +0530

    UI fixes (#23)
    * use next/image
    
    * use <img> for profile picture
    
    * fix: cobe canvas border radius
    
    * use next/image for profile pictures
    
    * add some capitals to cobe globe
    
    * picture fallback color
    
    * add cdn to images config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+      domains: [
+        'cdn.hello.coop', //Hellō Generic Profile Picture
+        'pictures.hello.coop',
+        'www.gravatar.com'
+      ]
+    }
 }
 
 module.exports = nextConfig
