f7adcb9d8cbac8a0618fa18a0ad785c5b87e6edb
Dick Hardt <dick.hardt@gmail.com>
2023-10-13 14:35:44 -0700

    improved example

--- src/loggedin.ts
+++ src/loggedin.ts
@@ -1,5 +1,5 @@
 
-import type { LoggedInParams, LoggedInResponse  } from '@hellocoop/nextjs'
+import type { LoggedInParams, LoggedInResponse } from '@hellocoop/nextjs'
 
 
 const loggedIn = async function ({ token, payload, req, res }: LoggedInParams): Promise<LoggedInResponse> {
@@ -12,13 +12,15 @@ const loggedIn = async function ({ token, payload, req, res }: LoggedInParams): 
     //      can be used to set cookies, or handle response
     //      if response has sent, return isProcessed=true in response
     // 
+    const { name, email, picture, sub } = payload // getting claims from payload
     const response = {
         // accessDenied: true   - will delete auth cookie
         // isProcessed: true    - if this function has handled the response
-        // auth: {              - customize what is returned from useAuth() and getAuth() 
-        //     ... properties for auth cookie
+        // auth: {              // customize what is returned from useAuth() and getAuth()
+        //     name, email, picture,   // defaults
+        //     role: 'admin'           // additional property
         // }
-        //
+        
     }
     return response
 }
