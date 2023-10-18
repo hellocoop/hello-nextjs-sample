b1896a9cb3f0cc1cd4269c2005f0d902b43f47dc
rohanharikr <rohanharikumar80@gmail.com>
2023-10-18 23:17:19 +0530

    hello favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,9 @@ export default function Document() {
             <Head>
                 <meta name="title" content="Hellō Next.js Starter"/>
                 <meta name="description" content="Login and registration in seconds"/>
+                <link rel="icon" href="https://cdn.hello.coop/images/favicon-light.png"  media="(prefers-color-scheme: light)" />
+                <link rel="icon" href="https://cdn.hello.coop/images/favicon-dark.png" media="(prefers-color-scheme: dark)" />
+                
                 <link rel="stylesheet" href="https://cdn.hello.coop/css/hello-btn.css"/>
             </Head>
             <body>
