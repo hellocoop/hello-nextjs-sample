6924205e433288ddb96071cbfa93a30e6da9f2ed
rohanharikr <rohanharikumar80@gmail.com>
2023-10-18 16:38:48 +0530

    remove meta utf tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
     return (
         <Html>
             <Head>
-                <meta charSet="UTF-8"/>
                 <meta name="title" content="Hellō Next.js Starter"/>
                 <meta name="description" content="Login and registration in seconds"/>
                 <link rel="stylesheet" href="https://cdn.hello.coop/css/hello-btn.css"/>
