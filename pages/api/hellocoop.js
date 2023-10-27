89a5ecaf1218f5c0eed699636254867440c0a14a
Dick Hardt <dick.hardt@hello.coop>
2023-10-27 10:39:37 -0700

    hello.config

--- pages/api/hellocoop.js
+++ pages/api/hellocoop.js
@@ -1,9 +1,20 @@
-import { pageAuth } from '@hellocoop/nextjs'
-import loggedIn from '@/src/loggedin' // skeleton for logged in
+// import { pageAuth } from '@hellocoop/nextjs'
+// import loggedIn from '@/src/loggedin' // skeleton for logged in
 
-const config = {
-    callbacks: {
-        loggedIn
-    }
+// const config = {
+//     callbacks: {
+//         loggedIn
+//     }
+// }
+// export default pageAuth(config)
+
+import { pageAuth } from '@hellocoop/nextjs'
+let config
+try {
+    config = await import('@hello.config')
+} catch (error) {
+    console.error('Could not import hello.config') 
+    process.exit(1)   
 }
-export default pageAuth(config)
+export default pageAuth(config)    
+
