b90c2053584deb90eb5046b2382f84ed7bff36ea
dickhardt <dick.hardt@hello.coop>
2024-05-24 11:19:52 -0700

    pin packages and fix signature

--- src/loggedin.ts
+++ src/loggedin.ts
@@ -1,8 +1,8 @@
 
-import type { LoggedInParams, LoggedInResponse } from '@hellocoop/nextjs'
+import type { LoginSyncParams, LoginSyncResponse } from '@hellocoop/nextjs'
 
 
-const loggedIn = async function ({ token, payload, cbReq, cbRes }: LoggedInParams): Promise<LoggedInResponse> {
+const loggedIn = async function ({ token, payload, cbReq, cbRes }: LoginSyncParams): Promise<LoginSyncResponse> {
     // token: string - a compact OpenID Connect ID Token  
     // payload: Claims - the parsed and typed payload from the ID Token
     //      eg. payload.sub
@@ -13,7 +13,7 @@ const loggedIn = async function ({ token, payload, cbReq, cbRes }: LoggedInParam
     //      res.setCookie()
     // 
     // const { name, email, picture, sub } = payload // getting claims from payload
-    const response: LoggedInResponse = {
+    const response: LoginSyncResponse = {
     //     // accessDenied: true,   // - will delete auth cookie
     //     // target_uri:           // - will redirect to target_uri if provided
     //     updatedAuth: {              // customize what is returned from useAuth() and getAuth()
