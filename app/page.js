74bec25c989d5f2bce8a996596a58bcafedb8d20
rohanharikr <rohanharikumar80@gmail.com>
2025-04-08 16:41:43 +0100

    add comments for client retrieve auth

--- app/page.js
+++ app/page.js
@@ -24,3 +24,30 @@ export default async function HomePage() {
     </HelloProvider>
   );
 }
+
+// client retrieves auth
+
+// 'use client'
+
+// import Hero from "@/components/Hero";
+// import Prompt from "@/components/Prompt";
+// import Info from "@/components/Info";
+// import Layout from "@/components/Layout";
+
+// import { ContinueButton, LoggedIn, LoggedOut } from "@hellocoop/nextjs/react"
+// export default function Home() {  
+//   return (
+//     <Layout>
+//       <LoggedIn>
+//           <Hero/>
+//       </LoggedIn>
+//       <LoggedOut>
+//           <Prompt/>
+//           {/* [ ō Continue with Hellō ] */}
+//           <ContinueButton theme="aware-static" hover="flare" providerHint="google github gitlab" />
+//           {/* Default <ContinueButton color="black" theme="ignore-light" hover="pop" />  */}
+//       </LoggedOut>
+//       <Info/>
+//     </Layout>
+//   )
+// }
