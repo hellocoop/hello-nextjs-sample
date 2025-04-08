431910027dc330beb8fbf1b386037656314877f6
rohanharikr <rohanharikumar80@gmail.com>
2025-04-08 16:29:29 +0100

    updates

---
+++ src/loggedin.ts
@@ -0,0 +1,25 @@
+import type { LoginSyncParams, LoginSyncResponse } from '@hellocoop/nextjs'
+
+const loggedIn = async function ({ token, payload }: LoginSyncParams): Promise<LoginSyncResponse> {
+    // token: string - a compact OpenID Connect ID Token  
+    // payload: Claims - the parsed and typed payload from the ID Token
+    //      eg. payload.sub
+    // req: CallbackRequest,
+    //      req.getHeaders()
+    // res: CallbackResponse
+    //      res.setHeader()
+    //      res.setCookie()
+    // 
+    // const { name, email, picture, sub } = payload // getting claims from payload
+    const response: LoginSyncResponse = {
+    //     // accessDenied: true,   // - will delete auth cookie
+    //     // target_uri:           // - will redirect to target_uri if provided
+    //     updatedAuth: {              // customize what is returned from useAuth() and getAuth()
+    //         name, email, picture,   // defaults
+    //         role: 'admin'           // additional property
+    //     }
+    }
+    return response
+}
+
+export default loggedIn
